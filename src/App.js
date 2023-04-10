d798859fb5562019a3224eeaf2365a23877f61c0
Bakka08 <samihsaadlo@gmail.com>
2023-04-10 02:39:26 +0000

    pvp

--- src/App.js
+++ src/App.js
@@ -14,16 +14,31 @@ function App() {
     height: "70px",
   };
   
+
   const [showFirstDiv, setShowFirstDiv] = useState(true);
   const [showSecondDiv, setShowSecondDiv] = useState(false);
   const [showThirdDiv, setShowThirdDiv] = useState(false);
   const [showFourthDiv, setShowFourthDiv] = useState(false);
+  const [showFifthDiv, setShowFifthhDiv] = useState(false);
+  const [w, setw] = useState(false);
+  const [d, setd] = useState(false);
   
+  function handw () {
+    setw(true);
+    setd(false);
+  };
+  function handd () {
+    setd(true);
+    setw(false);
+  }
+
   const handleShowFirst = () => {
     setShowFirstDiv(true);
     setShowSecondDiv(false);
     setShowThirdDiv(false);
     setShowFourthDiv(false);
+    setShowFifthhDiv(false);
+    setBoard((Array(9).fill(null)));
   };
 
   const handleShowSecondDiv = () => {
@@ -31,6 +46,8 @@ function App() {
     setShowSecondDiv(true);
     setShowThirdDiv(false);
     setShowFourthDiv(false);
+    setShowFifthhDiv(false);
+    setBoard((Array(9).fill(null)));
   };
   
   const handleShowThirdDiv = () => {
@@ -38,6 +55,8 @@ function App() {
     setShowSecondDiv(false);
     setShowThirdDiv(true);
     setShowFourthDiv(false);
+    setShowFifthhDiv(false);
+    setBoard((Array(9).fill(null)));
   };
   
   const handleShowFourthDiv = () => {
@@ -45,13 +64,129 @@ function App() {
     setShowSecondDiv(false);
     setShowThirdDiv(false);
     setShowFourthDiv(true);
+    setShowFifthhDiv(false);
+    setBoard((Array(9).fill(null)));
   };
+
+  function showFidthDiv() {
+    setShowFirstDiv(false);
+    setShowSecondDiv(false);
+    setShowThirdDiv(false);
+    setShowFourthDiv(false);
+    setShowFifthhDiv(true);
+    setBoard((Array(9).fill(null)));
+  }
+ function d3(){
+  
+      setShowFirstDiv(false);
+    setShowSecondDiv(false);
+    setShowThirdDiv(true);
+    setShowFourthDiv(false);
+    setShowFifthhDiv(false);
+ }
+  const count = () => {{
+    if(win === selectedValue){
+      setcountWin(counWin + 1);
+      d3();
+    setBoard((Array(9).fill(null)));
+    }else if(win === output){
+      setcountLose(counLose + 1);
+      d3();
+    }else {
+      setcountDraw(counDraw + 1);
+      d3();
+    }
+
+  }}
+ const quite = () => {{
+  setShowFirstDiv(true);
+  setShowSecondDiv(false);
+  setShowThirdDiv(false);
+  setShowFourthDiv(false);
+  setShowFifthhDiv(false);
+  setBoard((Array(9).fill(null)));
+  setcountDraw=0;
+  setwin('null');
+  setcountLose=0;
+  setcountWin=0;
+
+
+ }}
   
   const [selectedValue, setSelectedValue] = useState("X");
+  const output = (selectedValue === 'X') ? 'O' : 'X';
+  const [counWin, setcountWin] = useState(0);
+  const [counDraw, setcountDraw] = useState(0);
+  const [counLose, setcountLose] = useState(0);
+  const [win , setwin] = useState(null);
 
   const handleButtonClick = (value) => {
     setSelectedValue(value);
   };
+  function calculateWinner(board) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+    return null;
+  }const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+
+  function handleClick(index) {
+    // check if the square has already been clicked
+    if (board[index] !== null) {
+      return;
+    }
+    
+    const newBoard = [...board];
+    newBoard[index] = isXNext ? 'X' : 'O';
+    setBoard(newBoard);
+    setIsXNext(!isXNext);
+  }
+  
+
+  function renderSquare(index) {
+    return (
+      <button 
+        onClick={() => handleClick(index)}
+        className="btn btn-primary text-uppercase fs-1 fw-bold" type="button" style={{ height: "150px", marginLeft: "10px", marginRight: "10px", width: "150px", backgroundColor: "none",borderRadius:"25px" }}
+      >
+        {board[index]}
+      </button>
+    );
+  }
+
+  const winner = calculateWinner(board);
+
+  let status;
+  if (winner) { 
+    setwin(winner)
+    showFidthDiv();
+    handw();
+    status = `Winner: ${winner}`;
+   
+  } else if (board.every((square) => square !== null)) {
+    status = "It's a tie!";
+    showFidthDiv();
+    setwin('tie')
+    handd();
+
+  } else {
+    status = ` ${isXNext ? 'X' : 'O'}`;
+  }
+  
   
   return (
     <div id="bd" style={gif}>
@@ -176,7 +311,7 @@ function App() {
         </div>
       </div>;
 
-      <div id="vscomp" style={{ display: showSecondDiv ? 'block' : 'none' }}>
+      <div id="vsplayer" style={{ display: showThirdDiv   ? 'block' : 'none' }}>
   <div>
     <div className="container">
       <div
@@ -202,7 +337,7 @@ function App() {
               marginTop: "10px",
             }}
             disabled>
-            <span style={{ color: "rgb(171, 24, 166)" }}>X </span>TURN
+            <span style={{ color: "rgb(171, 24, 166)" }}>{status} </span>TURN
           </button>
           <button
             className="btn btn-primary fw-bold text-start"
@@ -222,146 +357,32 @@ function App() {
           />
         </div>
         <div style={{ height: "70%" }}>
-          <div style={{ marginTop: "30px" }}>
-            <button
-              className="btn btn-primary text-uppercase fs-1 fw-bold"
-              type="button"
-              style={{
-                height: "150px",
-                marginLeft: "14px",
-                marginRight: "10px",
-                width: "150px",
-                borderRadius: "0px",
-                backgroundColor: "#134cae",
-                textAlign: "center",
-              }}
-            >
-              <strong>X</strong>
-            </button>
-            <button
-              className="btn btn-primary text-uppercase fs-1 fw-bold"
-              type="button"
-              style={{
-                height: "150px",
-                marginLeft: "10px",
-                marginRight: "10px",
-                width: "150px",
-                borderRadius: "0px",
-                backgroundColor: "#134cae",
-              }}
-            >
-              X
-            </button>
-            <button
-              className="btn btn-primary text-uppercase fs-1 fw-bold"
-              type="button"
-              style={{
-                height: "150px",
-                marginLeft: "10px",
-                marginRight: "10px",
-                width: "150px",
-                borderRadius: "0px",
-                backgroundColor: "#134cae",
-              }}
-            >
-              X
-            </button>
-          </div>
-          <div style={{ marginTop: "10px" }}>
-            <button
-              className="btn btn-primary text-uppercase fs-1 fw-bold"
-              type="button"
-              style={{
-                height: "150px",
-                marginLeft: "14px",
-                marginRight: "10px",
-                width: "150px",
-                borderRadius: "0px",
-                backgroundColor: "#134cae",
-              }}
-            >
-              X
-            </button>
-            <button
-              className="btn btn-primary text-uppercase fs-1 fw-bold"
-              type="button"
-              style={{
-                height: "150px",
-                marginLeft: "10px",
-                marginRight: "10px",
-                width: "150px",
-                borderRadius: "0px",
-                backgroundColor: "#134cae",
-              }}
-            >
-              X
-            </button>
-            <button
-              className="btn btn-primary text-uppercase fs-1 fw-bold"
-              type="button"
-              style={{
-                height: "150px",
-                marginLeft: "10px",
-                marginRight: "border-radius: 0px",
-                backgroundColor: "#134cae",
-                width: "150px",
-                borderRadius: "0px",
-              }}
-            >
-              X
-            </button>
-          </div>
-          <div style={{ marginTop: "10px" }}>
-            <button
-              className="btn btn-primary text-uppercase fs-1 fw-bold"
-              type="button"
-              style={{
-                height: "150px",
-                marginLeft: "14px",
-                marginRight: "10px",
-                width: "150px",
-                borderRadius: "0px",
-                backgroundColor: "#134cae",
-              }}
-            >
-              X
-            </button>
-            <button
-              className="btn btn-primary text-uppercase fs-1 fw-bold"
-              type="button"
-              style={{
-                height: "150px",
-                marginLeft: "10px",
-                marginRight: "10px",
-                width: "150px",
-                borderRadius: "0px",
-                backgroundColor: "#134cae",
-              }}
-            >
-              X
-            </button>
-            <button
-              className="btn btn-primary text-uppercase fs-1 fw-bold"
-              type="button"
-              style={{
-                height: "150px",
-                marginLeft: "10px",
-                marginRight: "10px",
-                width: "150px",
-                borderRadius: "0px",
-                backgroundColor: "#134cae",
-              }}
-            >
-              X
-            </button>
-          </div>
+        <div>
+      <br></br>
+      <div className="board-row" style={{marginBottom: '10px'}}>
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row" style={{marginBottom: '10px'}}>
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row" >
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  
           <div style={{ marginTop: "10px" }}>
             <button
               className="btn btn-primary disabled"
               type="button"
               style={{
                 height: "100px",
-                marginLeft: "15px",
+                marginLeft: "11px",
                 marginRight: "10px",
                 width: "150px",
                 backgroundColor: "white",
@@ -369,8 +390,23 @@ function App() {
               }}
               disabled
             >
-              &nbsp; &nbsp; &nbsp; &nbsp;<strong> X (You)&nbsp; </strong>&nbsp;
-              &nbsp; &nbsp; &nbsp; &nbsp;#&nbsp;&nbsp;
+             <strong>&nbsp;{selectedValue}(PLAYER1)&nbsp; {counWin}</strong></button>
+            <button
+              className="btn btn-primary disabled"
+              type="button"
+              style={{
+                height: "100px",
+                marginLeft: "10px",
+                marginRight: "10px",
+                width: "150px",
+                backgroundColor: "white",
+                color: "black",
+              }}
+              disabled
+            >
+            
+              <strong>&nbsp;(TIES)&nbsp; {counDraw}</strong>
+              
             </button>
             <button
               className="btn btn-primary disabled"
@@ -385,34 +421,17 @@ function App() {
               }}
               disabled
             >
-              <strong>
-                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Ties&nbsp; &nbsp; &nbsp;
-                &nbsp; &nbsp; &nbsp; #
-              </strong>
-            </button>
-            <button
-              className="btn btn-primary disabled"
-              type="button"
-              style={{
-                height: "100px",
-                marginLeft: "10px",
-                marginRight: "10px",
-                width: "150px",
-                backgroundColor: "white",
-                color: "black",
-              }}
-              disabled
-            >
-              <strong>&nbsp;O(COMPUTER)&nbsp; #</strong>
+              <strong>&nbsp;{output}(PLAYER2)&nbsp; {counLose}</strong>
             </button>
           </div>
         </div>
       </div>
     </div>
   </div>
+  
       </div>;
 
-      <div id="vsplayer" style={{ display: showThirdDiv ? 'block' : 'none' }}>
+      <div id="vscomp" style={{ display: showSecondDiv ? 'block' : 'none' }}>
   <div>
     <div className="container">
       <div
@@ -673,6 +692,21 @@ function App() {
   
       </div>
 
+      <div id="main" className="container" style={{ display: showFifthDiv ? 'block' : 'none' }}>
+  
+
+      <div className="container">
+        <div style={{width: '30%', margin: 'auto', height: '150px', marginBottom: '20px', backgroundColor: 'none', borderRadius: '35px'}}>
+          <h4 className="font-monospace fw-bold text-center" style={{marginTop: '20px', display: w  ? 'block' : 'none' }}>{win}&nbsp;TAKES THE ROUND</h4>
+          <h4 className="font-monospace fw-bold text-center" style={{marginTop: '20px', display: d  ? 'block' : 'none' }}>&nbsp;it's a tie</h4>
+          <div style={{display: 'flex', alignItems: 'center'}}>
+            <button className="btn btn-primary" type="button" style={{margin: '0 10px', width: '30%', borderRadius: '0px', backgroundColor: 'var(--bs-white)', color: 'black', border: '0px'}} onClick={handleShowFirst}><strong>Quite</strong></button>
+            <button className="btn btn-primary" type="button" style={{margin: '0 10px', width: '80%', borderRadius: '0px', backgroundColor: '#31ccee', color: 'black', border: '0px'}} onClick={count}><strong>Next Round</strong></button></div>
+        </div>
+      </div>
+
+       
+      </div>;
     </div>
   );
 }
